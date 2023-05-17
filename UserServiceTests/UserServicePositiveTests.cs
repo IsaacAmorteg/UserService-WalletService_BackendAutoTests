a1f209f8fd6a8642d99ead5fa95c449a66fbc210
Isaac Amortegui <isaacamortegc@outlook.com>
2023-05-17 15:51:19 -0500

    Code refactoring

--- UserServiceTests/UserServicePositiveTests.cs
+++ UserServiceTests/UserServicePositiveTests.cs
@@ -6,31 +6,21 @@ using System.Text;
 
 namespace UserServicePositiveTests
 {
+    [TestFixture]
     public class UserServicePositiveTests
     {
+        private HttpClient client;
         [SetUp]
         public void Setup()
         {
+            client = new HttpClient();
         }
 
         [Test]
         public async Task T1_UserService_RegisterUser_WithEmptyFields_StatusCodeIs200()
         {
-            HttpClient client = new HttpClient();
-
-            UserServiceRegisterNewUserRequest requestBody = new UserServiceRegisterNewUserRequest()
-            {
-                firstName = " ",
-                lastName = " "
-            };
-
-            HttpRequestMessage request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new System.Uri("https://userservice-uat.azurewebsites.net/Register/RegisterNewUser"),
-                Content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json")
-            };
-
+            HttpRequestMessage request = CreateRegisterUserRequest("", "");
+            
             HttpResponseMessage response = await client.SendAsync(request);
 
             Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
@@ -38,20 +28,7 @@ namespace UserServicePositiveTests
         [Test]
         public async Task T2_UserService_RegisterUser_WithEmptyFields_ResponseIsIdGreaterThan0()
         {
-            HttpClient client = new HttpClient();
-
-            UserServiceRegisterNewUserRequest requestBody = new UserServiceRegisterNewUserRequest()
-            {
-                firstName = " ",
-                lastName = " "
-            };
-
-            HttpRequestMessage request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new System.Uri("https://userservice-uat.azurewebsites.net/Register/RegisterNewUser"),
-                Content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json")
-            };
+            HttpRequestMessage request = CreateRegisterUserRequest("", "");
 
             HttpResponseMessage response = await client.SendAsync(request);
             var content = await response.Content.ReadAsStringAsync();
@@ -59,7 +36,24 @@ namespace UserServicePositiveTests
 
             Assert.That(newUserId, Is.GreaterThan(0));
         }
+        private HttpRequestMessage CreateRegisterUserRequest(string firstName, string lastName)
+        {
+            UserServiceRegisterNewUserRequest requestBody = new UserServiceRegisterNewUserRequest()
+            {
+                firstName = firstName,
+                lastName = lastName
+            };
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new System.Uri("https://userservice-uat.azurewebsites.net/Register/RegisterNewUser"),
+                Content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json")
+            };
+            return request;
+
+        }
     }
+   
     public class UserServiceRegisterNewUserRequest
     {
         public string? firstName;
